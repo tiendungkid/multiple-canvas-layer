d4d49693b3c1e98dfcbb2a4459c65eed363bd295
Tiến Dũng <phantiendung29111999@gmail.com>
2022-10-11 01:06:03 +0700

    First commit

---
+++ src/coordinates-generator.js
@@ -0,0 +1,16 @@
+const shapeCoordinates = []
+
+function getCursorPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    shapeCoordinates.push(x, y)
+    console.log(shapeCoordinates)
+}
+
+setTimeout(() => {
+    const canvas = document.querySelector('canvas')
+    canvas.addEventListener('click', function (e) {
+        getCursorPosition(canvas, e)
+    })
+}, 3e3)
